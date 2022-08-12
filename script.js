b3556cdd0f8726ee0c1474f6b9fe904dc0d9644c
Carina Perez <carinaperez@live.com>
2022-08-11 23:04:24 -0300

    Primera Preentrega Proyecto

--- script.js
+++ script.js
@@ -14,44 +14,68 @@ function ingreso () {
         }
     }
 
-    let numeroEncuesta = " ";
-    while (numeroEncuesta === " " || numeroEncuesta === null){
-        numeroEncuesta = parseInt(prompt("Ingrese número de encuesta."));
-    }
-
-    let correo = " ";
-    while (correo === " " || correo === null){
-        correo = prompt("Ingrese un correo electrónico de contacto.");
-    }
-
     confirm("Ya podemos empezar. Estás listo?");
 
 }
 
 ingreso ()
 
-class DatosEncuesta {
-    constructor(linea, estacionOn, estacionOff, hora, edad, motivo) {
+
+class Registro {
+    constructor(id, edad, correo, telefono, linea, estacionOn, estacionOff, hora, motivo) {
+    this.id = id;
+    this.edad = edad;
+    this.correo = correo;
+    this.telefono = telefono;
     this.linea = linea;
     this.estacionOn = estacionOn;
     this.estacionOff = estacionOff;
     this.hora = hora;
-    this.edad = edad;
     this.motivo = motivo;
     }
 }
 
-const respuestasDatos = [];
-respuestasDatos.push(new DatosEncuesta("A", "Acoyte", "Congreso", "10 am", "20 años", "Estudio"));
-respuestasDatos.push(new DatosEncuesta("C", "Constitución", "Diagonal Norte", "8 am", "35 años", "Trabajo"));
+const datosEncuestados = [];
+datosEncuestados.push(new Registro("001", "20 años", "leo@gmail.com", "123456789", "A", "Acoyte", "Congreso", "10 am", "Estudio"));
+datosEncuestados.push(new Registro("002", "35 años", "juana@gmail.com", "789456123", "C", "Constitución", "Diagonal Norte", "8 am",  "Trabajo"));
+datosEncuestados.push(new Registro("003", "43 años", "maxi@gmail.com", "456789123", "B", "Florida", "Rosas", "17 pm",  "Trabajo"));
 
-for (const datos of respuestasDatos)
-    respuestasDatos.reverse ();
+console.table (datosEncuestados);
+   
+const nuevoEncuestado = {
+    id: "004",
+    edad: prompt("Ingresá tu edad: "),
+    correo: prompt("Ingresá tu correo electrónico: "),
+    telefono: prompt("Ingresá tu teléfono: "),
+    linea: prompt("¿En qué línea te encontrás?"),
+    estacionOn: prompt("¿En qué estación ingresaste al Subte?"),
+    estacionOff: prompt("¿En qué estación salís del Subte?"),
+    hora: prompt("¿A qué hora ingresaste al Subte?"),
+    motivo: prompt("Elegí el motivo de tu viaje: trabajo - estudio - compras - salud - otros")
+};
+
+datosEncuestados.push(nuevoEncuestado);
+
+console.table (datosEncuestados);
+
+function buscarEstacion (){
+    let estacionBuscada = prompt ("¿Qué estación querés buscar?")
+    const resultado = datosEncuestados.find (estacion => estacion.estacionOn === estacionBuscada)
+    console.table (resultado)
+    }
+
+buscarEstacion ()
+
+debugger
+
+let respuesta1
+let respuesta2
+let respuesta3
+let respuesta4
     
-
 function seccionA () {
 
-    let respuesta1 = prompt("1- En general, ¿cómo calificás el servicio de Subte de la Ciudad de Buenos Aires? \n1-Muy bueno \n2-Bueno \n3-Regular \n4-Malo \n5-Muy malo \n9-NS/NC");
+    respuesta1 = prompt("1- En general, ¿cómo calificás el servicio de Subte de la Ciudad de Buenos Aires? \n1-Muy bueno \n2-Bueno \n3-Regular \n4-Malo \n5-Muy malo \n9-NS/NC");
 
     while (respuesta1 != "1" && respuesta1 != "2" && respuesta1 != "3"  && respuesta1 != "4" && respuesta1 != "5" && respuesta1 != "9"){
         alert ("Elegí una de las opciones ofrecidas.");
@@ -59,34 +83,39 @@ function seccionA () {
     }
    
 
-    let respuesta2 = prompt("2- ¿Cuál de estos medios de transporte público creés que brinda un mejor servicio? \n1-Subte \n2-Colectivo \n3-Tren \n4-Metrobús \n5-Premetro \n7-Otros (especificar al final)");
+    respuesta2 = prompt("2- ¿Cuál de estos medios de transporte público creés que brinda un mejor servicio? \n1-Subte \n2-Colectivo \n3-Tren \n4-Metrobús \n5-Premetro \n7-Otros (especificar al final)");
 
-        while(respuesta2 != "1" && respuesta2 != "2" && respuesta2 != "3"  && respuesta2 != "4" && respuesta2 != "5" && respuesta2 != "7") {
+    while(respuesta2 != "1" && respuesta2 != "2" && respuesta2 != "3"  && respuesta2 != "4" && respuesta2 != "5" && respuesta2 != "7") {
         alert ("Elegí una de las opciones ofrecidas.");
         respuesta2 = prompt("2- ¿Cuál de estos medios de transporte público creés que brinda un mejor servicio? \n1-Subte \n2-Colectivo \n3-Tren \n4-Metrobús \n5-Premetro \n7-Otros (especificar al final)")
-        }
+    }
 
-        if (respuesta2 === "7"){
+    if (respuesta2 === "7"){
             let respuesta2a = prompt("Ingrese un medio de transporte.") 
-        }
+    }
 
-    let respuesta3 = prompt("3- ¿Utilizaste alguna vez la red WiFi del subte? \n1-Sí \n2-No");
+    respuesta3 = prompt("3- ¿Utilizaste alguna vez la red WiFi del subte? \n1-Sí \n2-No");
 
-        while (respuesta3 != "1" && respuesta3 != "2") {
+    while (respuesta3 != "1" && respuesta3 != "2") {
         alert ("Elegí una de las opciones ofrecidas.");
         respuesta3 = prompt("3- ¿Utilizaste alguna vez la red WiFi del subte? \n1-Sí \n2-No")
-        }
+    }
 
-        if (respuesta3 === "1"){
-            let respuesta4 = prompt("4-Cómo calificarías el servicio de WiFI? \n1-Excelente \n2-Bueno \n3-Regular \n4-Malo")
-        }
+    if (respuesta3 === "1"){
+            respuesta4 = prompt("4-Cómo calificarías el servicio de WiFI? \n1-Excelente \n2-Bueno \n3-Regular \n4-Malo")
+    }
 
-        while (respuesta4 != "1" && respuesta4 != "2" && respuesta4 != "3" && respuesta4 != "4"){
+    while (respuesta4 != "1" && respuesta4 != "2" && respuesta4 != "3" && respuesta4 != "4"){
             alert ("Elegí una de las opciones ofrecidas.");
             respuesta4 = prompt("4-Cómo calificarías el servicio de WiFI? \n1-Excelente \n2-Bueno \n3-Regular \n4-Malo")
-        }
-
+    }
 }
 
 seccionA ()
+
+const respuestasEncuestado001 = []
+
+respuestasEncuestado001.push(respuesta1, respuesta2, respuesta3, respuesta4)
+
+console.table (respuestasEncuestado001)
 
---
+++ variables.js
@@ -0,0 +1,9 @@
+const nuevoEncuestado = {}
+
+const opciones1 = ["1-Muy bueno", "2-Bueno", "3-Regular", "4-Malo", "5-Muy malo", "9-NS/NC"];
+
+const opciones2 = ["1-Subte", "2-Colectivo", "3-Tren", "4-Metrobús", "5-Premetro", "7-Otros (especificar al final)"]
+
+const opciones3 = ["1-Si", "2-No"]
+
+const opciones4 = ["1-Excelente", "2-Bueno", "3-Regular", "4-Malo"]
